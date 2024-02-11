61d0a62f91988505da57f0c883e413fb036135ff
Olokodana AbdulKabir <Olokodanaabdulkabir@gmail.com>
2024-02-11 11:40:46 +0100

    completed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,14 @@ const hamburgerMenu = document.querySelector('.hamburger')
 const closeMenu = document.querySelector('.close')
 const menuContainer = document.querySelector('.menu_container')
 
+var sections = document.querySelectorAll('.sections')
+var navLinks = document.querySelectorAll('header nav a')
+
+
+//active navigation item
+
+
+
 // Mobile Navigation Menu Function
 const MenuFunc = ()=>{
   // Icon style 
@@ -13,3 +21,23 @@ const MenuFunc = ()=>{
 }
 
 hamburgerMenu.addEventListener('click', MenuFunc)
+
+
+// Active item on Navigation panel 
+
+ window.onscroll = () => {
+   sections.forEach(sec =>{
+     let top = window.scrollY
+     let offset = sec.offsetTop - 60
+     let height = sec.offsetHeight
+     let id = sec.getAttribute('id')
+    
+     if (top >= offset && top < offset + height) {
+       navLinks.forEach(link =>{
+         link.classList.remove('is-active')
+         document.querySelector(`.desktop_view a[href = '#${id}']`).classList.add('is-active')
+         document.querySelector(`.menu_container a[href = '#${id}']`).classList.add('is-active')
+       })
+     }
+   })
+ }
